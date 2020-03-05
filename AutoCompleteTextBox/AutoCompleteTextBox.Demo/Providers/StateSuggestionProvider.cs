94fcdf35f89428663c20570719b35eedab81699a
Ryan Van Slooten <ryanvs@yahoo.com>
2020-03-05 17:25:15 -0600

    Enhanced Demo to include WebColors suggestion and DataGrid Master-Detail.

--- AutoCompleteTextBox/AutoCompleteTextBox.Demo/Providers/StateSuggestionProvider.cs
+++ AutoCompleteTextBox/AutoCompleteTextBox.Demo/Providers/StateSuggestionProvider.cs
@@ -7,26 +7,37 @@ using AutoCompleteTextBox.Editors;
 
 namespace AutoCompleteTextBox.Demo.Providers
 {
-    
-        public class StateSuggestionProvider : ISuggestionProvider
+    public class StateSuggestionProvider : ISuggestionProvider
+    {
+        public IEnumerable<State> ListOfStates { get; set; }
+
+        public State GetExactSuggestion(string filter)
         {
-            public IEnumerable<State> ListOfStates { get; set; }
-
-            public IEnumerable GetSuggestions(string filter)
-            {
-                if (string.IsNullOrWhiteSpace(filter)) return null;
-                return
-                    ListOfStates
-                        .Where(state => state.Name.StartsWith(filter, StringComparison.CurrentCultureIgnoreCase))
-                        .ToList();
-
-            }
-
-            public StateSuggestionProvider()
-            {
-                var states = StateFactory.CreateStateList();
-                ListOfStates = states;
-            }
+            if (string.IsNullOrWhiteSpace(filter)) return null;
+            return
+                ListOfStates
+                    .FirstOrDefault(state => string.Equals(state.Name, filter, StringComparison.CurrentCultureIgnoreCase));
         }
-    
+
+        public IEnumerable<State> GetSuggestions(string filter)
+        {
+            if (string.IsNullOrWhiteSpace(filter)) return null;
+            System.Threading.Thread.Sleep(1000);
+            return
+                ListOfStates
+                    .Where(state => state.Name.IndexOf(filter, StringComparison.CurrentCultureIgnoreCase) > -1)
+                    .ToList();
+        }
+
+        IEnumerable ISuggestionProvider.GetSuggestions(string filter)
+        {
+            return GetSuggestions(filter);
+        }
+
+        public StateSuggestionProvider()
+        {
+            var states = StateFactory.CreateStateList();
+            ListOfStates = states;
+        }
+    }
 }
