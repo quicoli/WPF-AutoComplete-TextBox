3e2d0cb8cecb5919909f7631f0b743112b9b5471
Ryan Van Slooten <ryanvs@yahoo.com>
2020-06-26 17:27:41 -0500

    Added Hyperlink to repository in Demo. Fixed State DebuggerDisplay.

--- AutoCompleteTextBox/AutoCompleteTextBox.Demo/MainWindow.xaml.cs
+++ AutoCompleteTextBox/AutoCompleteTextBox.Demo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 
 namespace AutoCompleteTextBox.Demo
@@ -27,6 +28,14 @@ namespace AutoCompleteTextBox.Demo
         private void Titlebar_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            // Open the associated browser to the repository page
+            var psi = new ProcessStartInfo(e.Uri.AbsoluteUri);
+            Process.Start(psi);
+            e.Handled = true;
         }
     }
 }
